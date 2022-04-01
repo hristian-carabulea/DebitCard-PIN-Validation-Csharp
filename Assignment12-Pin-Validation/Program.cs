046c1808cd71ce42ab2ee2f46dd0ac9bc3f24d7b
hristian-carabulea <hristian.carabulea@outlook.com>
2022-04-01 14:12:02 +0200

    first commit

---
+++ Assignment12-Pin-Validation/Program.cs
@@ -0,0 +1,17 @@
+﻿class AssignmentPINValidation
+{
+    static void Main()
+    {
+        DebitCard debitCard = new DebitCard();
+
+        System.Console.Write("Enter a 4-digit or 6-digit PIN: ");
+        debitCard.PIN = System.Console.ReadLine();
+
+        //debitCard.PIN = "12345"; //Output: PIN contains one or more non-digit characters.
+        System.Console.WriteLine(debitCard.PIN); //Output: null (shown as blank)
+
+        System.Console.WriteLine("Enter any key to end ");
+        System.Console.ReadKey();
+
+    }
+}
